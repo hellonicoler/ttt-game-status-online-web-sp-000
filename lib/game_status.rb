c1be666b03cf3f0e250732dd7939bd9d29c85a29
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-09 20:54:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,3 +56,18 @@ def over?(board)
     return true
   end
 end
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else 
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return
+      "O"
+    end
+  end
+end
